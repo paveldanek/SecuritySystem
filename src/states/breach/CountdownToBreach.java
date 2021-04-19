ca5b85542c29c9e346bf232a7e4eba54e8f53f65
unknown <79498618+benhines1119@users.noreply.github.com>
2021-04-19 00:54:08 -0500

    Added some functionality for 6
    CountdownToBreach:
    -triggers a timer on entering the state
    -breach when the TimerRanOut state is entered
    -enters Ready state when EnterPassword handleEvent is invoked on it
    
    ArmedAway:
    -enters CountdownToBreach state when UncheckZone event or PressMotion event is called on it

--- src/states/breach/CountdownToBreach.java
+++ src/states/breach/CountdownToBreach.java
@@ -1,7 +1,71 @@
 package states.breach;
 
+import events.EnterPassword;
+import events.TimerRanOut;
 import states.Countdown;
+import states.Ready;
+import states.SecurityContext;
+import timer.Timer;
 
 public class CountdownToBreach extends Countdown {
+	private static CountdownToBreach instance;
+	protected Timer timer;
 
+	/**
+	* Private for the singleton pattern
+	*/
+	protected CountdownToBreach() {
+		this.enter();
+	}
+
+	/**
+	* For singleton
+	* 
+	* @return the object
+	*/
+	public static CountdownToBreach instance() {
+		if (instance == null) {
+			instance = new CountdownToBreach();
+		}
+		return instance;
+	}
+
+	public void startTimer() {
+		timer = new Timer(this, 60);
+	}
+
+	public void setTimer(Timer timer) {
+		this.timer = new Timer(this, timer.getTimeValue());
+	}
+
+	public int getTimeValue() {
+		return timer.getTimeValue();
+	}
+	
+	/**
+	 * Processes the password being entered
+	 */
+	public void handleEvent(EnterPassword event) {
+		SecurityContext.instance().changeState(Ready.instance());
+		this.leave();
+	}
+	
+	/**
+	 * Processes the timer running out
+	 */
+	public void handleEvent(TimerRanOut event) {
+		SecurityContext.instance().changeState(Breach.instance());
+		this.leave();
+	}
+
+	@Override
+	public void enter() {
+		this.startTimer();
+	}
+
+	@Override
+	public void leave() {
+		timer.stop();
+		timer = null;
+	}
 }
